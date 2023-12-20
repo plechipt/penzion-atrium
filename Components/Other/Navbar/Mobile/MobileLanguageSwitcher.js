8d80137544392c21ac9464ee1f2c3642dac97fc6
Jakub Plecháč <plechac.k@gmail.com>
2023-12-20 20:06:15 +0100

    switching between languages

--- Components/Other/Navbar/Mobile/MobileLanguageSwitcher.js
+++ Components/Other/Navbar/Mobile/MobileLanguageSwitcher.js
@@ -7,14 +7,22 @@ import {
 } from "@/Components/ui/select.jsx";
 import { CzechRIcon, USFlagIcon } from "@/assets/icons";
 
+import { useRouter } from "@/navigation";
+
 const languagesMobile = [
+  { code: "cs", name: "CZ", flag: <CzechRIcon className="w-6" /> },
   { code: "en", name: "EN", flag: <USFlagIcon className="w-6" /> },
-  { code: "cz", name: "CZ", flag: <CzechRIcon className="w-6" /> },
 ];
 
 const MobileLanguageSwitcher = () => {
+  const router = useRouter();
+
+  const handleLanguageChange = (value) => {
+    router.push("/", { locale: value });
+  };
+
   return (
-    <Select defaultValue={"en"}>
+    <Select onValueChange={handleLanguageChange} defaultValue="cs">
       <SelectTrigger className="w-full border-0 border-transparent bg-neutral-50 rounded-lg">
         <SelectValue
           className="w-full flex items-center gap-2 font-bold"
