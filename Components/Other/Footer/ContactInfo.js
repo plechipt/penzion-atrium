eb39736ccb626f132bf3ed8c39e6b2dd1dc935f2
Jakub Plecháč <plechac.k@gmail.com>
2023-12-22 01:01:40 +0100

    put translation in code in Footer component

--- Components/Other/Footer/ContactInfo.js
+++ Components/Other/Footer/ContactInfo.js
@@ -1,20 +1,23 @@
+import { useTranslations } from "next-intl";
 import { LocationIcon, PhoneIcon } from "@/assets/icons";
 
 const ContactInfo = () => {
+  const t = useTranslations("Contact");
+
   return (
     <div className="space-y-3 lg:text-start text-center flex items-center flex-col lg:items-start">
-      <h3 className="text-base font-bold">Contact info</h3>
+      <h3 className="text-base font-bold">{t("contactInfo")}</h3>
 
       <div className="flex items-center gap-2">
         <LocationIcon className="w-5 h-5 text-[#fff]" />
         <p className="text-white text-base font-normal leading-relaxed">
-          541 01 - Trutnov, Žižkova 504
+          {t("location")}
         </p>
       </div>
       <div className="flex items-center gap-2">
         <PhoneIcon className="w-5 h-5 text-[#fff]" />
         <p className="text-white text-base font-normal leading-relaxed">
-          tel.: (+420) 731 415 455
+          {t("telephone")}
         </p>
       </div>
     </div>
