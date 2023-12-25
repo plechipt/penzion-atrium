37c9d16d013f3d1d924449406b2c3b396df4d717
Jakub Plecháč <plechac.k@gmail.com>
2023-12-25 14:35:40 +0100

    loop on surroundings page

--- Components/Surroundings Section/Surroundings.js
+++ Components/Surroundings Section/Surroundings.js
@@ -1,69 +1,25 @@
-import { useTranslations } from "next-intl";
-
-import imgAPI from "@/public/images/ImageApi";
 import SurroundingElement from "./SurroundingElement";
+import SurroundingsData from "@/data/SurroundingsData";
 
 const Surroundings = () => {
-  const t = useTranslations("Surroundings");
+  const { title, surrondings } = SurroundingsData();
 
   return (
     <div className="container py-10 lg:py-10">
       <h1 className="md:text-5xl text-4xl font-bold mb-8 lg:mb-10 text-center">
-        {t("title")}
+        {title}
       </h1>
-      <SurroundingElement
-        heading={t("trutnovTitle")}
-        text={t("trutnovDescription")}
-        large_img={imgAPI.surr_img[6]}
-        small_img={imgAPI.surr_img[0]}
-        reverse={false}
-        link="https://www.ictrutnov.cz/je-na-draka"
-      />
-
-      <SurroundingElement
-        heading={t("krkonoseTitle")}
-        text={t("krkonoseDescription")}
-        large_img={imgAPI.surr_img[7]}
-        small_img={imgAPI.surr_img[1]}
-        reverse={true}
-        link="https://www.krnap.cz"
-      />
-
-      <SurroundingElement
-        heading={t("adrspachTitle")}
-        text={t("adrspachDescription")}
-        large_img={imgAPI.surr_img[8]}
-        small_img={imgAPI.surr_img[2]}
-        reverse={false}
-        link="https://www.adrspasskeskaly.cz"
-      />
-
-      <SurroundingElement
-        heading={t("kuksTitle")}
-        text={t("kuksDescription")}
-        large_img={imgAPI.surr_img[9]}
-        small_img={imgAPI.surr_img[3]}
-        reverse={true}
-        link="https://www.hospital-kuks.cz"
-      />
-
-      <SurroundingElement
-        heading={t("zooTitle")}
-        text={t("zooDescription")}
-        large_img={imgAPI.surr_img[10]}
-        small_img={imgAPI.surr_img[4]}
-        reverse={false}
-        link="https://safaripark.cz"
-      />
-
-      <SurroundingElement
-        heading={t("dolceTitle")}
-        text={t("dolceDescription")}
-        large_img={imgAPI.surr_img[10]}
-        small_img={imgAPI.surr_img[5]}
-        reverse={true}
-        link="https://www.camp-dolce.cz"
-      />
+      {surrondings.map((item, index) => (
+        <SurroundingElement
+          key={index}
+          heading={item.heading}
+          text={item.text}
+          large_img={item.large_img}
+          small_img={item.small_img}
+          reverse={item.reverse}
+          link={item.link}
+        />
+      ))}
     </div>
   );
 };
--- data/SurroundingsData.js
+++ data/SurroundingsData.js
@@ -1,5 +1,63 @@
-const SurroundingsData = () => {
-  return <div>SurroundingsData</div>;
+import imgAPI from "@/public/images/ImageApi";
+import { useTranslations } from "next-intl";
+
+const SurrondingsData = () => {
+  const t = useTranslations("Surroundings");
+
+  const title = t("title");
+
+  const surrondings = [
+    {
+      heading: t("trutnovTitle"),
+      text: t("trutnovDescription"),
+      large_img: imgAPI.surr_img[6],
+      small_img: imgAPI.surr_img[0],
+      reverse: false,
+      link: "https://www.ictrutnov.cz/je-na-draka",
+    },
+    {
+      heading: t("krkonoseTitle"),
+      text: t("krkonoseDescription"),
+      large_img: imgAPI.surr_img[7],
+      small_img: imgAPI.surr_img[1],
+      reverse: true,
+      link: "https://www.krnap.cz",
+    },
+    {
+      heading: t("adrspachTitle"),
+      text: t("adrspachDescription"),
+      large_img: imgAPI.surr_img[8],
+      small_img: imgAPI.surr_img[2],
+      reverse: false,
+      link: "https://www.adrspasskeskaly.cz",
+    },
+    {
+      heading: t("kuksTitle"),
+      text: t("kuksDescription"),
+      large_img: imgAPI.surr_img[9],
+      small_img: imgAPI.surr_img[3],
+      reverse: true,
+      link: "https://www.hospital-kuks.cz",
+    },
+    {
+      heading: t("zooTitle"),
+      text: t("zooDescription"),
+      large_img: imgAPI.surr_img[10],
+      small_img: imgAPI.surr_img[4],
+      reverse: false,
+      link: "https://safaripark.cz",
+    },
+    {
+      heading: t("dolceTitle"),
+      text: t("dolceDescription"),
+      large_img: imgAPI.surr_img[10],
+      small_img: imgAPI.surr_img[5],
+      reverse: true,
+      link: "https://www.camp-dolce.cz",
+    },
+  ];
+
+  return { title, surrondings };
 };
 
-export default SurroundingsData;
+export default SurrondingsData;
