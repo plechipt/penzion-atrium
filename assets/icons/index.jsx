a392715844ef767506fce8b0ad9d53344d4aa6b0
Jakub Plecháč <plechac.k@gmail.com>
2023-12-25 23:17:22 +0100

    renamed images

--- assets/icons/index.jsx
+++ assets/icons/index.jsx
@@ -1,58 +1,58 @@
-export { default as BookNowIcon } from "../svg/book_now.svg";
-export { default as MainLogoIcon } from "../svg/main_logo.svg";
+export { default as BookNowIcon } from "@/assets/svg/book_now.svg";
+export { default as MainLogoIcon } from "@/assets/svg/main_logo.svg";
 
 // Our Amenities
-export { default as FreeWifiIcon } from "../svg/free_wifi.svg";
-export { default as GardenWithPlaygroundIcon } from "../svg/garden_with_playground.svg";
-export { default as BedsIcon } from "../svg/beds_from_200.svg";
-export { default as FreeParkingIcon } from "../svg/free_parking.svg";
-export { default as WheelchairIcon } from "../svg/wheelchair_accessible.svg";
-export { default as BusIcon } from "../svg/bus.svg";
-export { default as GroupDiscountsIcon } from "../svg/group_discounts.svg";
-export { default as TVIcon } from "../svg/tv.svg";
+export { default as FreeWifiIcon } from "@/assets/svg/free_wifi.svg";
+export { default as GardenWithPlaygroundIcon } from "@/assets/svg/garden_with_playground.svg";
+export { default as BedsIcon } from "@/assets/svg/beds_from_200.svg";
+export { default as FreeParkingIcon } from "@/assets/svg/free_parking.svg";
+export { default as WheelchairIcon } from "@/assets/svg/wheelchair_accessible.svg";
+export { default as BusIcon } from "@/assets/svg/bus.svg";
+export { default as GroupDiscountsIcon } from "@/assets/svg/group_discounts.svg";
+export { default as TVIcon } from "@/assets/svg/tv.svg";
 
 // Our Accommodations
-export { default as BathtubIcon } from "../svg/bathtub.svg";
-export { default as TVSquareIcon } from "../svg/tv_square.svg";
-export { default as ToiletIcon } from "../svg/toilet.svg";
-export { default as FridgeIcon } from "../svg/fridge.svg";
-export { default as CupIcon } from "../svg/cup.svg";
-export { default as SupperIcon } from "../svg/supper.svg";
-export { default as TransportIcon } from "../svg/transport.svg";
-export { default as LaundryIcon } from "../svg/laundry.svg";
-export { default as TVRoundedIcon } from "../svg/tv_rounded.svg";
+export { default as BathtubIcon } from "@/assets/svg/bathtub.svg";
+export { default as TVSquareIcon } from "@/assets/svg/tv_square.svg";
+export { default as ToiletIcon } from "@/assets/svg/toilet.svg";
+export { default as FridgeIcon } from "@/assets/svg/fridge.svg";
+export { default as CupIcon } from "@/assets/svg/cup.svg";
+export { default as SupperIcon } from "@/assets/svg/supper.svg";
+export { default as TransportIcon } from "@/assets/svg/transport.svg";
+export { default as LaundryIcon } from "@/assets/svg/laundry.svg";
+export { default as TVRoundedIcon } from "@/assets/svg/tv_rounded.svg";
 
-export { default as ImageIcon } from "../svg/image.svg";
+export { default as ImageIcon } from "@/assets/svg/image.svg";
 
 // Sports
-export { default as SportsHallIcon } from "../svg/sports_hall.svg";
-export { default as AthleticStadiumIcon } from "../svg/atheletics_stadium.svg";
-export { default as MultipurposeGymIcon } from "../svg/multipurpose_gym.svg";
-export { default as MultipurposeCourtIcon } from "../svg/multipurpose_court.svg";
-export { default as GymnasticHallIcon } from "../svg/gymnastics_hall.svg";
-export { default as VolleyBallHallIcon } from "../svg/volleyball_court.svg";
-export { default as FitnessGymIcon } from "../svg/fitness_gym.svg";
-export { default as AsphaltCircuitIcon } from "../svg/asphalt_circuit.svg";
-export { default as ClimbingWallIcon } from "../svg/climbing_hall.svg";
-export { default as SkiTrailsIcon } from "../svg/ski_trails.svg";
-export { default as SkiResortsIcon } from "../svg/ski_park.svg";
+export { default as SportsHallIcon } from "@/assets/svg/sports_hall.svg";
+export { default as AthleticStadiumIcon } from "@/assets/svg/atheletics_stadium.svg";
+export { default as MultipurposeGymIcon } from "@/assets/svg/multipurpose_gym.svg";
+export { default as MultipurposeCourtIcon } from "@/assets/svg/multipurpose_court.svg";
+export { default as GymnasticHallIcon } from "@/assets/svg/gymnastics_hall.svg";
+export { default as VolleyBallHallIcon } from "@/assets/svg/volleyball_court.svg";
+export { default as FitnessGymIcon } from "@/assets/svg/fitness_gym.svg";
+export { default as AsphaltCircuitIcon } from "@/assets/svg/asphalt_circuit.svg";
+export { default as ClimbingWallIcon } from "@/assets/svg/climbing_hall.svg";
+export { default as SkiTrailsIcon } from "@/assets/svg/ski_trails.svg";
+export { default as SkiResortsIcon } from "@/assets/svg/ski_park.svg";
 
 // Contact Icons
-export { default as LocationIcon } from "../svg/location.svg";
-export { default as PhoneIcon } from "../svg/call-calling.svg";
-export { default as SMSIcon } from "../svg/sms.svg";
+export { default as LocationIcon } from "@/assets/svg/location.svg";
+export { default as PhoneIcon } from "@/assets/svg/call-calling.svg";
+export { default as SMSIcon } from "@/assets/svg/sms.svg";
 
 // Footer logo
-export { default as FooterLogoIcon } from "../svg/footer_logo.svg";
-export { default as FooterLogoTextIcon } from "../svg/footer_logo_text.svg";
+export { default as FooterLogoIcon } from "@/assets/svg/footer_logo.svg";
+export { default as FooterLogoTextIcon } from "@/assets/svg/footer_logo_text.svg";
 
 // Social Icons
-export { default as FacebookIcon } from "../svg/facebook.svg";
-export { default as InstagramIcon } from "../svg/instagram.svg";
+export { default as FacebookIcon } from "@/assets/svg/facebook.svg";
+export { default as InstagramIcon } from "@/assets/svg/instagram.svg";
 
-export { default as UserIcon } from "../svg/user.svg";
-export { default as UserIcon2 } from "../svg/user2.svg";
+export { default as UserIcon } from "@/assets/svg/user.svg";
+export { default as UserIcon2 } from "@/assets/svg/user2.svg";
 
 // Flags
-export { default as USFlagIcon } from "../svg/united-states-flag-icon.svg";
-export { default as CzechRIcon } from "../svg/czech-republic-flag-icon.svg";
+export { default as USFlagIcon } from "@/assets/svg/united-states-flag-icon.svg";
+export { default as CzechRIcon } from "@/assets/svg/czech-republic-flag-icon.svg";
