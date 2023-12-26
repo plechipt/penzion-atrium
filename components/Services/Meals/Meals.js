dfe73d50829d54738eeefad4b73f8888b17accf6
Jakub Plecháč <plechac.k@gmail.com>
2023-12-26 12:17:14 +0100

    renamed images from getImages

--- components/Services/Meals/Meals.js
+++ components/Services/Meals/Meals.js
@@ -8,7 +8,7 @@ const Meals = () => {
     <div className="flex flex-col lg:flex-row items-center mb-4 gap-6 lg:gap-14">
       {/* Left Part with Image */}
       <ImageEffect
-        img={imgAPI.ser_img[3]}
+        img={MeetingRoomImage}
         imgAlt={"Meeting Room"}
         divClass={"relative w-full lg:max-w-[440px] h-72 lg:h-[437px]"}
         imgClass={"w-full h-full object-cover rounded-[10px]"}
