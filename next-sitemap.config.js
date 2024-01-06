add752d79b0175ba5e8bde1c82cb3411448cd32e
Jakub Plecháč <plechac.k@gmail.com>
2024-01-06 14:04:22 +0100

    added alternateRefs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,14 @@ const dev = process.env.NODE_ENV !== "production";
 module.exports = {
   siteUrl: dev ? "http://localhost:3000" : "https://penziontrutnov.cz",
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: "https://www.penziontrutnov.cz/cs",
+      hreflang: "cs",
+    },
+    {
+      href: "https://www.penziontrutnov.cz/en",
+      hreflang: "en",
+    },
+  ],
 };
