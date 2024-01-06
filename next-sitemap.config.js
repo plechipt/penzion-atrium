006bbbdc285b14213c6996b3115c5258f9869f39
Jakub Plecháč <plechac.k@gmail.com>
2024-01-06 14:09:25 +0100

    added transform function

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,4 +15,14 @@ module.exports = {
       hreflang: "en",
     },
   ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
