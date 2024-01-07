61f3e0a20f27e5dbaa72195a8291486000550490
Jakub Plecháč <plechac.k@gmail.com>
2024-01-07 23:26:16 +0100

    manually generate sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,47 @@
+export default function sitemap() {
+  return [
+    // Czech
+    {
+      url: "https://penziontrutnov.cz/cs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/cs/ubytovani",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/cs/sluzby",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/cs/okoli",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/cs/kontakt",
+      lastModified: new Date(),
+    },
+
+    // English
+    {
+      url: "https://penziontrutnov.cz/en",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/en/accommodation",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/en/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/en/surroundings",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.penziontrutnov.cz/en/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
