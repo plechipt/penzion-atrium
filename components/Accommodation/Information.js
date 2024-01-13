9b7cedad603cdfcc8f0f012b3ae612f80566b4c3
Jakub Plecháč <plechac.k@gmail.com>
2024-01-13 14:47:20 +0100

    🎨️ edited city tax

--- components/Accommodation/Information.js
+++ components/Accommodation/Information.js
@@ -15,7 +15,7 @@ const Information = () => {
         {text2.map((word, index) => (
           <span
             key={index}
-            className={word === "20Kč" ? "text-indigo-900 font-bold" : ""}
+            className={word === "40Kč" ? "text-indigo-900 font-bold" : ""}
           >
             {word}{" "}
           </span>
