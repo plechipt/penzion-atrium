2372bed3cd4c9e428db19f8ce05d46b968827f26
Jakub Plecháč <plechac.k@gmail.com>
2024-01-15 19:23:28 +0100

    added social links

--- components/Other/Footer/LogoWithSocials.js
+++ components/Other/Footer/LogoWithSocials.js
@@ -12,16 +12,18 @@ const LogoWithSocials = () => {
 
       <div className="lg:flex hidden items-center gap-3">
         <Link
+          href="https://www.instagram.com/penzionatriumtrutnov"
+          target="_blank"
           className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
-          href="#"
-        >
-          <FaFacebookF />
-        </Link>
-        <Link
-          className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
-          href="#"
         >
           <FaInstagram />
+        </Link>
+        <Link
+          href="https://www.facebook.com/penzionatriumtrutnov"
+          target="_blank"
+          className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
+        >
+          <FaFacebookF />
         </Link>
       </div>
     </div>
