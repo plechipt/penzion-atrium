d5184a0aaee94bf4344d0a61682c6945515132b5
Jakub Plecháč <plechac.k@gmail.com>
2024-01-16 18:13:24 +0100

    🎨️ edited logos in footer on mobile devices

--- components/Other/Footer/MobileIcons.js
+++ components/Other/Footer/MobileIcons.js
@@ -5,16 +5,18 @@ const MobileIcons = () => {
   return (
     <div className="flex lg:hidden items-center gap-3">
       <Link
+        href="https://www.instagram.com/penzionatriumtrutnov"
+        target="_blank"
         className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
-        href="#"
-      >
-        <FaFacebookF />
-      </Link>
-      <Link
-        className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
-        href="#"
       >
         <FaInstagram />
+      </Link>
+      <Link
+        href="https://www.facebook.com/penzionatriumtrutnov"
+        target="_blank"
+        className="w-9 h-9 rounded-full border border-stone-300 flex items-center justify-center text-stone-300 hover:bg-secondary-gradient hover:border-0 transition-all duration-300"
+      >
+        <FaFacebookF />
       </Link>
     </div>
   );
