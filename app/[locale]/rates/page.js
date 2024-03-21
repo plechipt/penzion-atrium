f7e3c760e0602c3e839d336b11afdeec98da12c6
Jakub Plecháč <plechac.k@gmail.com>
2024-03-21 23:37:45 +0100

    🔧 added Table component

--- app/[locale]/rates/page.js
+++ app/[locale]/rates/page.js
@@ -1,5 +1,10 @@
 import ContactUs from "@/components/Contact/ContactUs";
+import Table from "@/components/Rates/Table";
 
 export default function Page() {
-  return <main className="lg:container">test</main>;
+  return (
+    <main className="lg:container">
+      <Table />
+    </main>
+  );
 }
